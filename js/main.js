4836c4f299429ec32080737b26508b8a1921455f
Artur199207 <artcomputers92@gmail.com>
2024-04-03 23:49:56 +0400

    dior

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.top__swiper', {
     effect: 'fade',
     autoplay:{
         delay:3500,
         disableOnInteraction:false,
     },
 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+  const swiperAboute = new Swiper(".aboute__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
